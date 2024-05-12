4db76784b016e4fc1750d78156347694a1f1caeb
alexandra-perez <alexandraperezny@gmail.com>
2024-05-12 15:38:06 -0400

    add description to showVideo

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -1,5 +1,8 @@
 import "./CommentForm.scss";
 
-export default function CommentForm() {
-    
+export default function CommentForm({video}) {
+    return (
+        <div className="CommentForm">
+        </div>
+    )
 }
--- src/components/ShowVideo/ShowVideo.jsx
+++ src/components/ShowVideo/ShowVideo.jsx
@@ -1,6 +1,7 @@
-import './ShowVideo.scss';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import CommentForm from '../CommentForm/CommentForm';
+import './ShowVideo.scss';
 
 export default function ShowVideo() {
   const { videoId } = useParams();
@@ -10,7 +11,6 @@ export default function ShowVideo() {
   }`;
 
   useEffect(() => {
-    console.log(singleVideoUrl)
     fetch(singleVideoUrl)
       .then((res) => res.json())
       .then((res) => {
@@ -21,28 +21,28 @@ export default function ShowVideo() {
 
   return (
     <div className="showVideo">
-      <div className="showVideo__window"></div>
-      <iframe
-        width="560"
-        height="315"
-        src={`https://www.youtube.com/embed/${videoId}`}
-        title={video.items ? video.items[0].snippet.title : 'YouTube video'}
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
-      {video.items && <h2>{video.items[0].snippet.title}</h2>}
-      <div className="showVideo__description">
-        {video.items && <h3>{video.items[0].snippet.description}</h3>}
+      <div className="showVideo__window">
+        <iframe
+          width="560"
+          height="315"
+          src={`https://www.youtube.com/embed/${videoId}`}
+          title={video.items ? video.items[0].snippet.title : 'YouTube video'}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <div className="title">
+        {video.items && <h2>{video.items[0].snippet.title}</h2>}
+      </div>
+      <div className="channelTitle">
+        {video.items && <h3>{video.items[0].snippet.channelTitle}</h3>}
+      </div>
+      <div className="description">
+        {video.items && <h4>{video.items[0].snippet.description}</h4>}
+      </div>
+      <div className="comments">
+        <CommentForm video={video} />
       </div>
     </div>
   );
 }
-    //  <iframe
-    //    width="560"
-    //    height="315"
-    //    src={`https://www.youtube.com/embed/${video.id.videoID}`}
-    //    title="YouTube video player"
-    //    frameBorder="0"
-    //    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    //    allowFullScreen
-    //  ></iframe>;
