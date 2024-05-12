a3e86cab0352af2a236065cafe07750aa8be4678
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-05-12 15:40:58 -0400

    add navigate after search

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,14 @@
 import "./SearchBar.scss";
+import { useNavigate } from "react-router-dom";
 
 export default function SearchBar({ setSearch }) {
+    const navigate = useNavigate()
 
     function handleSearch (e) {
         e.preventDefault()
         setSearch(e.target.search.value)
+        e.target.search.value = ""
+        navigate("/")
     }
 
     return (
