923448ddea78e9e6a18d46709e50aa06e3cc98d3
alexandra-perez <alexandraperezny@gmail.com>
2024-05-12 11:14:48 -0400

    add video info to videoList

--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -10,6 +10,7 @@ export default function VideoList({ videoList }) {
           <Link to={`/videos/${video.id}`}>
             <div>
               <img src={video.snippet.thumbnails.default.url} alt="" />
+              <h2>{video.snippet.title}</h2>
           </div>
           </Link>
         ))}
