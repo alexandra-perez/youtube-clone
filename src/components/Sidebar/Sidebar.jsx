2206780208e7e576dbf34b9b2faebf3749fbea55
ayoublos <abelibel@pursuit.org>
2024-05-14 20:41:38 -0700

    finished sideb before pulling

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,20 +1,27 @@
 import { useEffect, useState } from 'react';
 import './Sidebar.scss';
-import{Link} from 'react-router-dom'
+import{Link, useParams} from 'react-router-dom'
 
 // export default function Sidebar() {
 //     return (<div className='sidebar-container'>
 //     hello world
-
+    
 //     </div>)
 // }
 export default function Sidebar() {
+  function random(){
+    return Math.floor(Math.random()*30)
+  }
+  let number=random()
+
+
+  const { videoId } = useParams();
     const[sideBarVideos,setSideBarVideos]=useState([])
-    const mostPopularUrl = `https://www.googleapis.com/youtube/v3/videos?part=snippet&maxResults=4&chart=mostPopular&key=${
-    import.meta.env.VITE_API_KEY
-  }`;
+    const categoryUrl = `https://www.googleapis.com/youtube/v3/videos?part=snippet&maxResults=2&chart=mostPopular&videoCategoryId=${number}&key=${
+      import.meta.env.VITE_API_KEY
+    }`;
     useEffect(() => {
-        fetch(mostPopularUrl)
+        fetch(categoryUrl)
           .then((res) => res.json())
           .then((res) => {
             // console.log(res.items)
@@ -22,14 +29,15 @@ export default function Sidebar() {
             console.log(newObj)
             setSideBarVideos([...newObj]);
           });
-      }, []);
+      }, [videoId]);
 
     return (
       <div className='sidebar-container'>
           {sideBarVideos.map((video, i) => (
-            <Link to={`/videos/${video.id}`} onClick={() => {window.location.href=`/videos/${video.id}`}}>
+          
+            <Link to={`/videos/${video.id}`}>
               <div className='singleimage-container'>
-                <img src={video.snippet.thumbnails.high.url} alt="" />
+                <img src={video.snippet.thumbnails.default.url} alt="" />
                 <h2>{video.snippet.title}</h2>
               </div>
             </Link>
